2d1a5ed841a43fa00ba2dcf57d9ae21110380ddb
Robert Brem <robert.brem@adesso.ch>
2018-09-11 09:41:36 +0200

    initial commit

---
+++ src/main/java/ch/open/football/players/boundary/PlayersResource.java
@@ -0,0 +1,15 @@
+package ch.open.football.players.boundary;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+
+@Path("players")
+public class PlayersResource {
+
+
+    @GET
+    public String getPlayers() {
+        return "player1, player2";
+    }
+
+}
